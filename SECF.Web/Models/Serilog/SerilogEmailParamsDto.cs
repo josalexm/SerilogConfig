8baed82d194739b73f520b899389fb3183791804
Alexander Muñoz <jmunoz@bitworks.com.sv>
2021-02-13 16:04:20 -0600

    Configuracion de serilog en proyecto web

---
+++ SECF.Web/Models/Serilog/SerilogEmailParamsDto.cs
@@ -0,0 +1,41 @@
+﻿using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SECF.Web.Models.Serilog
+{
+    public class SerilogEmailParamsDto
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ApiKeySendGrid { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FromEmail { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FromName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> Destinations { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public LogEventLevel LogEventLevel { get; set; }
+    }
+}
--- SECF.Web/Startup.cs
+++ SECF.Web/Startup.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SECF.Web.Models.Serilog;
+using SendGrid;
+using Serilog;
+using Serilog.Sinks.Email;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +26,29 @@ namespace SECF.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Serilog configuration
+
+            LoggerConfiguration serilogConfiguration = new LoggerConfiguration();
+            SerilogEmailParamsDto serilogParams = Configuration.GetSection("SerilogEmailParams").Get<SerilogEmailParamsDto>();
+            serilogParams.Destinations.ForEach(destination => {
+                EmailConnectionInfo emailConnectionInfo = new EmailConnectionInfo
+                {
+                    EmailSubject = serilogParams.Subject,
+                    FromEmail = serilogParams.FromEmail,
+                    SendGridClient = new SendGridClient(serilogParams.ApiKeySendGrid),
+                    FromName = serilogParams.FromName,
+                    ToEmail = destination
+                };
+
+                serilogConfiguration.ReadFrom.Configuration(Configuration)
+                    .WriteTo.Email(emailConnectionInfo, restrictedToMinimumLevel: serilogParams.LogEventLevel, period: TimeSpan.FromMinutes(5));
+            });
+
+            Log.Logger = serilogConfiguration.CreateLogger();
+            services.AddLogging(configure => { configure.AddSerilog(); });
+
+            #endregion
+
             services.AddControllersWithViews();
         }
 
